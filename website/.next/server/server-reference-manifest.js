7822ffe056c11247aac9178dc04b06fabb51f2b2
hoffe <djkagemand1@gmail.com>
2025-03-20 01:50:30 +0100

    Version 1 of website

--- website/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"zbNje77ImUioxX8EgAhsjLn1fPms+wFINrOE05BMWxo=\"\n}"
--- website/.next/static/chunks/pages/_app.js
+++
@@ -1,9 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/node_modules_next_dist_a84eef54._.js",
-  "static/chunks/node_modules_react-dom_82bb97c6._.js",
-  "static/chunks/node_modules_1b7400a8._.js",
-  "static/chunks/[root of the server]__659f318b._.js",
-  "static/chunks/src_styles_90f219f6._.css",
-  "static/chunks/src_pages__app_5771e187._.js",
-  "static/chunks/src_pages__app_b3df1522._.js"
-])
